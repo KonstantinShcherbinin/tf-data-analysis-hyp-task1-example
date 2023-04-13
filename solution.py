c807ca694e531f404b2b1e8c9f92c07ccbabdcb4
k.scherbinin <k.scherbinin@digital-core.ru>
2023-04-13 17:29:22 +0500

    1 вариант

--- solution.py
+++ solution.py
@@ -14,12 +14,11 @@ def solution(x_success: int,
     # Не меняйте название функции и её аргументы
     #nobs = np.array([x_cnt, y_cnt])
     #count = np.array([x_success, y_success])
-    nobs = y_cnt
-    count = y_success
-    value = x_success / x_cnt
-    t, pval = proportions_ztest(count, nobs, value=value, alternative='two-sided')
-    #return t, pval
-    if pval < 0.1:
+    nobs = np.array([x_cnt, y_cnt])
+    count = np.array([x_success, y_success])
+    t, pval = proportions_ztest(count, nobs, value=0.1, alternative='smaller')
+    #return t
+    if t > -1.28:
         return True
     else:
         return False
