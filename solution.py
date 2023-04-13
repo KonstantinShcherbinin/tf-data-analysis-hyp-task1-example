08708886e9e8d09d9ad1a69b67640ade684b34e6
k.scherbinin <k.scherbinin@digital-core.ru>
2023-04-13 16:46:24 +0500

    один массив

--- solution.py
+++ solution.py
@@ -12,11 +12,14 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    nobs = np.array([x_cnt, y_cnt])
-    count = np.array([x_success, y_success])
-    t, pval = proportions_ztest(count, nobs, value=0.1, alternative='smaller')
-    #return t
-    if t > -1.28:
+    #nobs = np.array([x_cnt, y_cnt])
+    #count = np.array([x_success, y_success])
+    nobs = y_cnt
+    count = y_success
+    value = x_success / x_cnt
+    t, pval = proportions_ztest(count, nobs, value=value, alternative='smaller')
+    #return t, pval
+    if pval < 0.1:
         return True
     else:
         return False
